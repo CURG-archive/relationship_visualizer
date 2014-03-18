1ffdd436fbcc369ba2e5285624c0c443282021cb
Jamis Johnson <jamismanwaring@gmail.com>
2014-03-18 17:52:32 -0400

    Uniquely colors each segmented cloud
    TODO: consistently color segmented clouds between frames. right now
    the color can change if the list of segmented clouds changes.

--- src/relationship_visualizer_node.cpp
+++ src/relationship_visualizer_node.cpp
@@ -14,7 +14,8 @@ namespace relation_viz {
         private:
             ros::NodeHandle nHandle;
             ros::Publisher  segmentedPub;
-            ros::Subscriber sub;
+            ros::Subscriber segmentedSub;
+            std::vector<int32_t> colors;
             void segmentedObjsReceived(
                 const perception_msgs::SegmentedObjectList::ConstPtr &msg);
         public:
@@ -23,30 +24,53 @@ namespace relation_viz {
     };
 
     RelationViz::RelationViz(): nHandle("") {
-        sub = nHandle.subscribe("segmented_objects", 100, &RelationViz::segmentedObjsReceived, this);
+        segmentedSub = nHandle.subscribe("segmented_objects", 10, &RelationViz::segmentedObjsReceived, this);
         segmentedPub = nHandle.advertise<pcl::PointCloud<pcl::PointXYZRGB> > ("visualized_segmented_objects", 10);
+
+        // create vector of colors
+        int32_t rgb;
+        for (uint8_t r = 0; r <= 4; ++r) {
+            for (uint8_t g = 0; g <= 4; ++g) {
+                for (uint8_t b = 0; b <= 4; ++b) {
+                    rgb = (r*85 << 16) | (g*85 << 8) | b*85;
+                    colors.push_back(rgb);
+                }
+            }
+        }
+
         ROS_INFO("relation_viz node ready");
     }
 
     void RelationViz::segmentedObjsReceived(const perception_msgs::SegmentedObjectList::ConstPtr &msg) {
         ROS_INFO("received segmented object list message");
 
-        std::vector<perception_msgs::SegmentedObject> segmentedMsgs = msg.get()->segmentedObjects;
-        uint8_t r = 255, g = 0, b = 0;
-        int32_t rgb = (r << 16) | (g << 8) | b;
+        // add all segments to one cloud before publishing
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr segments (new pcl::PointCloud<pcl::PointXYZRGB>);
 
+        // iterate through all segmented cloud and uniquely color each
+        int32_t rgb;
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr segmented (new pcl::PointCloud<pcl::PointXYZRGB>);
+        std::vector<perception_msgs::SegmentedObject> segmentedMsgs = msg.get()->segmentedObjects;
         for (int i = 0; i< segmentedMsgs.size(); i++) {
-            // sensor_msg::PointCloud2 -> pcl::PointCloud<PointXYZRGB>
-            pcl::PointCloud<pcl::PointXYZRGB> *segmented = new pcl::PointCloud<pcl::PointXYZRGB>();
+
+            // get segmented cloud and convert to pcl::PointCloud
             pcl::fromROSMsg(segmentedMsgs[i].segmentedObjectPointCloud, *segmented);
-            // color segmented object
-            // TODO uniquely color, don't just color all red
-            for (int i = 0; i < segmented->points.size(); ++i) {
-                segmented->points[i].rgb = *reinterpret_cast<float*>(&rgb);
+
+            // add color to each point in segment
+            rgb = colors[i % colors.size()];
+            for (int j = 0; j < segmented->points.size(); ++j) {
+                segmented->points[j].rgb = *reinterpret_cast<float*>(&rgb);
             }
-            segmentedPub.publish(*segmented);
-            delete segmented;
+
+            // add colored segment to cloud being published
+            *segments += *segmented;
         }
+
+        // set frame
+        segments->header.frame_id = msg.get()->segmentedObjects[0].segmentedObjectPointCloud.header.frame_id;
+
+        // publish
+        segmentedPub.publish(segments);
     }
 }
 
